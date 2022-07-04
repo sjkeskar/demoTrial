4a00e847b48215c01dc8cab0266ebe54ce08e4f6
sjkeskar <sjkeskar@gmail.com>
2022-07-04 17:42:05 +0530

    tags updated

--- Login.java
+++ Login.java
@@ -1,5 +1,6 @@
 class Login {
     public Boolean loginNow(String username, String password){
+        System.out.println("Hello from login");
         return username.equalsIgnoreCase(password);
     } 
 }
