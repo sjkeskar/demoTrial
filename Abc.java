04df358b269b13d2e42e5785cc8dec83f0bd9701
sjkeskar <sjkeskar@gmail.com>
2022-07-08 17:20:02 +0530

    Calculator

--- Abc.java
+++ Abc.java
@@ -1,8 +1,13 @@
 
 public class Abc {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello Everybody");
-        }
+        System.out.println("Hello Everybody");
+        Calc c1= new Calc();
+        System.out.println(c1.add(10, 20));
+        System.out.println(c1.sub(30, 20));
+        System.out.println(c1.mul(400, 20));
+        System.out.println(c1.div(10, 20));
+        // System.out.println(c1.add(100, 20));
+        // System.out.println(c1.sub(100, 50));
     }
 }
