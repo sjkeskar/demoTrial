73a5307382fb673611c7db2edc8149bccf9e41a4
sjkeskar <sjkeskar@gmail.com>
2022-07-08 17:22:34 +0530

    out update

--- Abc.java
+++ Abc.java
@@ -3,11 +3,11 @@ public class Abc {
     public static void main(String[] args) {
         System.out.println("Hello Everybody");
         Calc c1= new Calc();
-        System.out.println(c1.add(10, 20));
-        System.out.println(c1.sub(30, 20));
-        System.out.println(c1.mul(400, 20));
-        System.out.println(c1.div(10, 20));
-        // System.out.println(c1.add(100, 20));
-        // System.out.println(c1.sub(100, 50));
+        System.out.println("Add "+c1.add(10, 20));
+        System.out.println("Sub "+c1.sub(30, 20));
+        System.out.println("Mul "+c1.mul(400, 20));
+        System.out.println("Div "+c1.div(10, 20));
+        System.out.println("Add "+c1.add(100, 20));
+        System.out.println("Sub "+c1.sub(100, 50));
     }
 }
